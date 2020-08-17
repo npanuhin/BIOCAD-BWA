7d69c35e9f9f220e6c60f88cca18b9a22627477a
npanuhin <79215987151@mail.ru>
2020-08-17 21:24:09 +0300

    k-mers update

--- Alignment/k-mers.cpp
+++ Alignment/k-mers.cpp
@@ -27,7 +27,7 @@ using namespace std;
 
 const int K = 3,
 	  	  HSSP = 1,
-	  	  SCORE_THRESOLD = 1;  // Warning! Others will not work!!!
+	  	  SCORE_THRESOLD = 3;
 
 int getKey(vector<char>::iterator start, vector<char>::iterator end) {
 	int res = 0;
@@ -68,25 +68,26 @@ int k_MersTest() {
 
 	cout << "Starting k-mers test..." << endl;
 
-	ifstream input_text2("samples/genome2.txt");
-	vector<char> text = readSeq(input_text2);
-	input_text2.close();
-	convertACGT(text);
+	ifstream input_database("samples/database.txt");
+	vector<char> database = readSeq(input_database);
+	int database_size = database.size();
+	input_database.close();
+	convertACGT(database);
 
 	vector<int> degrees_of_4 = {1};
 	for (int i = 1; i <= K; ++i) degrees_of_4.push_back(degrees_of_4[i - 1] * 4);
 
 	vector<pair<int, vector<int>>> pos;
-	int key = getKey(text.begin(), text.begin() + K - 1);
+	int key = getKey(database.begin(), database.begin() + K - 1);
 	key += 3 * degrees_of_4[K - 1];
 
 	bool found;
 	int found_ind;
 	vector<int> tmp_v_int;
 
-	for (int l = 0; l < (int)text.size() - K; ++l) {
+	for (int l = 0; l < database_size - K; ++l) {
 
-		key = (key % degrees_of_4[K - 1]) * 4 + text[l + K];
+		key = (key % degrees_of_4[K - 1]) * 4 + database[l + K - 1];
 
 		found = false;
 		for (found_ind = 0; found_ind < (int)pos.size(); ++found_ind) {
@@ -123,22 +124,39 @@ int k_MersTest() {
 			cout << reversed_key[j];
 		}
 		cout << ' ' << pos[i].second.size() << endl;
+
+		// for (int f = 0; f < pos[i].second.size(); ++f) {
+		// 	cout << pos[i].second[f] << ' ';
+		// }
+		// cout << endl;
 	}
 
-	ifstream input_text1("samples/genome1.txt");
-	vector<char> text2 = readSeq(input_text1);
-	input_text1.close();
-	convertACGT(text2);
+	ifstream input_query("samples/query.txt");
+	vector<char> query = readSeq(input_query);
+	int query_size = query.size();
+	input_query.close();
+	convertACGT(query);
 
-	int left, right, middle;
+	vector<int> matrix_line_one(query_size + 100), matrix_line_two(query_size + 100);
+	matrix_line_one[0] = 0; matrix_line_two[0] = 0;
+	vector<int>::iterator matrix_line_left = matrix_line_one.begin(), matrix_line_right = matrix_line_two.begin(), matrix_line_tmp;
+	bool matrix_column_reached_score, up_set;
 
-	long long count = 0;
+	int left, right, middle, y, x, up, bottom, highest_score, cur_value;
+	// long long count = 0;
 
-	// vector<vector<int>> hssp_matches(text2.size() - K);
-	// vector<pair<int, int>> seeds;
+	cout << "Seeding..." << endl;
 
-	for (int l = 0; l < (int)text2.size() - K; ++l) {
-		vector<int> keys = {getKey(text2.begin() + l, text2.begin() + l + K)};
+	ofstream result_file("result.txt");
+
+	for (int l = 0; l < query_size - K; ++l) {
+
+		// if (l % 10 == 0) {
+		// 	cout << "l = " << l << endl;
+		// }
+		cout << "l = " << l << endl;
+
+		vector<int> keys = {getKey(query.begin() + l, query.begin() + l + K)};
 
 		for (int key_ind = 0; key_ind < (int)keys.size(); ++key_ind) {
 			key = keys[key_ind];
@@ -150,19 +168,97 @@ int k_MersTest() {
 				if (pos[middle].first == key) {
 					for (int i = 0; i < (int)pos[middle].second.size(); ++i) {
 						// cout << l << ' ' << pos[middle].second[i] << endl;
-						// seeds.emplace_back(l, pos[middle].second[i]);
-						int x = l, y = pos[middle].second[i], cur_score = 0;
-						bool reached_score = false;
+						y = l;
+						x = pos[middle].second[i];
+						up = y; bottom = y;
+						highest_score = 0;
 
-						++count;
+						*(matrix_line_left + bottom + 1) = 0;
+						*(matrix_line_left + up) = 0;
+						*(matrix_line_right + up) = 0;
 
-						while (!reached_score || cur_score >= SCORE_THRESOLD) {
-							// TODO
-							break;
+						// cout << "x = " << x << " y = " << y << " | ";
+						// ++count;
+
+						result_file << x << ' ' << y << " -> ";
+
+						matrix_column_reached_score = true;
+
+						for (x = pos[middle].second[i]; matrix_column_reached_score && x < database_size; ++x) {
+							matrix_column_reached_score = false;
+
+							// cout << endl << "New x = " << x << endl;
+
+							up_set = false;
+							cur_value = -1;
+
+							for (y = up; (cur_value != 0 || y != bottom) && y < query_size; ++y) {
+								if (bottom == y) {
+									*(matrix_line_left + bottom + 1) = 0;
+									++bottom;
+								}
+
+								cur_value = max(max(
+									*(matrix_line_left + y + 1) - 1, // (y + 1)
+									*(matrix_line_right + y) - 1 // (y + 1) - 1
+								),
+									0
+								);
+
+								if (query[y] == database[x]) {
+									cur_value = max(
+										cur_value,
+										*(matrix_line_left + y) + 1 // (y + 1) - 1
+									);
+								} else {
+									cur_value = max(
+										cur_value,
+										*(matrix_line_left + y) - 1 // (y + 1) - 1
+									);
+								}
+
+								if (cur_value <= highest_score - SCORE_THRESOLD) {
+									cur_value = 0;
+								} else {
+									highest_score = max(highest_score, cur_value);
+								}
+
+								if (cur_value != 0) {
+									// cout << '(' << y << ',' << cur_value << ')';
+									matrix_column_reached_score = true;
+
+									if (!up_set) {
+										up_set = true;
+										up = max(up, y);
+									}
+								}
+
+								// cout << "y = " << y << endl << *(matrix_line_left + y) << ' ' << *(matrix_line_right + y) << endl << *(matrix_line_left + y + 1) << ' ' << cur_value << endl << endl;
+
+								*(matrix_line_right + y + 1) = cur_value; // (y + 1)
+
+								// if (cur_value == 0 && y + 1 == bottom) {
+								// 	// cout << "break" << ' ';
+								// 	// cout << '(' << *(matrix_line_right + y) << ')';
+								// 	break;
+								// }
+							}
+
+							matrix_line_tmp = matrix_line_left;
+							matrix_line_left = matrix_line_right;
+							matrix_line_right = matrix_line_tmp;
+
+							// cout << x << '|' << matrix_column_reached_score << ' ';
+
+							// if (!matrix_column_reached_score) break;
+
+							// break;
 						}
+
+						result_file << x << ' ' << y << " (" << highest_score << ')' << endl << endl;
+
+						// cout << endl << "------------------------------------------" << endl << endl;
 					}
-					
-					// hssp_matches[l].insert(hssp_matches[l].end(), pos[middle].second.begin(), pos[middle].second.end());
 					break;
 
 				} else if (key < pos[middle].first) {
@@ -174,11 +270,16 @@ int k_MersTest() {
 		}
 	}
 
-	cout << count << endl;
+	// cout << count << endl;
 
 	// for (int i = 0; i < hssp_matches.size(); ++i) {
 	// 	cout << hssp_matches[i].size() << endl;
 	// }
 
+	result_file.close();
+
 	cout << "Test End" << endl << endl;
 }
+
+
+// Ищем геном1 в геноме2
