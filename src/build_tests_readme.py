7731bfbf994987949e20a81eea68f624a480b7bf
npanuhin <79215987151@mail.ru>
2020-08-19 15:25:16 +0300

    Updated settings, added large7

---
+++ src/build_tests_readme.py
@@ -0,0 +1,49 @@
+import os
+
+
+def buildTests():
+    os.chdir("tests")
+
+    with open("README.md", 'w', encoding="utf-8") as readme:
+        print("<h1 align=\"center\">Tests</h1>\n", file=readme)
+
+        test_folders = [foldername for foldername in os.listdir() if os.path.isdir(foldername) and foldername != "small"]
+
+        headers = ["|", "BLAST", "My plot", "Lines with actions", "History GIF"]
+        table = []
+
+        for test_folder in test_folders:
+            # os.chdir(test_folder)
+
+            blast_link = "https://raw.githubusercontent.com/npanuhin/BIOCAD_BWA/master/tests/{}/BLAST.png".format(test_folder)
+            dot_plot_link = "https://raw.githubusercontent.com/npanuhin/BIOCAD_BWA/master/tests/{}/sam_analyze (dot plot).png".format(test_folder)
+            plot_link = "https://raw.githubusercontent.com/npanuhin/BIOCAD_BWA/master/tests/{}/sam_analyze.png".format(test_folder)
+            history_link = "https://raw.githubusercontent.com/npanuhin/BIOCAD_BWA/master/tests/{}/history.gif".format(test_folder)
+
+            table.append([
+                test_folder,
+                "[BLAST]({} \"View image\")".format(blast_link.replace(' ', "%20")),
+                "[My plot]({} \"View image\")".format(dot_plot_link.replace(' ', "%20")),
+                "[Lines with actions]({} \"View image\")".format(plot_link.replace(' ', "%20")),
+                "[History GIF]({} \"View GIF\")".format(history_link.replace(' ', "%20")),
+            ])
+
+            # os.chdir("../")
+
+        print("|".join(headers), file=readme)
+        print("|".join([":-:"] * len(headers)), file=readme)
+        for line in table:
+            print("|".join(line), file=readme)
+
+    os.chdir("../")
+
+
+def main(root_path):
+    start_path = os.getcwd()
+    os.chdir(root_path)
+    buildTests()
+    os.chdir(start_path)
+
+
+if __name__ == "__main__":
+    main("../")
