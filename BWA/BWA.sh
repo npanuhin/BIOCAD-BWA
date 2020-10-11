ef985c70cba04eeab715d292031243eb011ee4a1
npanuhin <79215987151@mail.ru>
2020-10-11 10:52:09 +0300

    Updated repository after recent changes

--- BWA/BWA.sh
+++ BWA/BWA.sh
@@ -15,15 +15,15 @@ bwa mem -o "bwa_output.sam" "bwa_genome1.fasta" "bwa_genome2.fasta"
 # bwa bwasw -N 1 -f "bwa_output.sam" "bwa_genome1.fasta" "bwa_genome2.fasta"
 printf "Aligned (.sam)\n\n"
 
-samtools view -Sb "bwa_output.sam" > "bwa_output.bam"
-printf "Converted to .bam\n\n"
+# samtools view -Sb "bwa_output.sam" > "bwa_output.bam"
+# printf "Converted to .bam\n\n"
 
-samtools sort -o "bwa_output_sorted.bam" "bwa_output.bam"
-printf "Sorted .bam\n\n"
+# samtools sort -o "bwa_output_sorted.bam" "bwa_output.bam"
+# printf "Sorted .bam\n\n"
 
-samtools index "bwa_output_sorted.bam"
-printf "Indexed .bam\n\n"
+# samtools index "bwa_output_sorted.bam"
+# printf "Indexed .bam\n\n"
 
 printf "Process time: $(( $(date +%s) - START_TIME ))s\n"
 
-sam2pairwise < bwa_output.sam > bwa_output_pairwise.txt
+# sam2pairwise < bwa_output.sam > bwa_output_pairwise.txt
